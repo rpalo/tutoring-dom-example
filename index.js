dc5dd868016587be5a7785f1f8b13360d5437463
Ryan Palo <ryan@thepalos.com>
2018-05-22 15:47:53 -0700

    Complete level 3: add pagination

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const parent = document.querySelector(".container");
+const parent = document.querySelector(".table__parent");
 // fake_data.js provides data, an array of objects
 
+const PAGESIZE = 50;
+
 build_data_table(parent, data);
+
 
 // Get handles on inputs
 const dateSearch = document.querySelector('.search[name="timestamp"]');
@@ -10,10 +13,16 @@ const selectSearches = document.querySelectorAll('select.search');
 const textSearches = document.querySelectorAll('input.search');
 const allSearches = document.querySelectorAll(".search");
 const clearButton = document.querySelector(".clear");
-const rows = document.querySelectorAll("tr");
+const rows = document.querySelectorAll(".data");
+const paginationParent = document.querySelector(".pagination");
+
+createPaginationLinks(data, PAGESIZE);
+
+render(PAGESIZE);
+
 
 // Setup inputs and add event listeners
-textSearches.forEach(input => input.addEventListener("keyup", filter_table));
+textSearches.forEach(input => input.addEventListener("keyup", () => render(PAGESIZE)));
 
 selectSearches.forEach(select => {
   const column = select.name;
@@ -23,32 +32,68 @@ selectSearches.forEach(select => {
     optionElement.text = option;
     select.options.add(optionElement);
   });
-  select.addEventListener("change", filter_table);
+  select.addEventListener("change", () => render(PAGESIZE));
 });
 
 clearButton.addEventListener("click", () => {
   allSearches.forEach(search => search.value = "");
-  show_all(rows);
+  render(PAGESIZE);
 });
 
 function filter_table() {
-  data.forEach((record, ind) => {
+  return data.map((record, ind) => {
     if (matches_all_searches(allSearches, record)) {
-      rows[ind + 1].style.display = "";
+      return ind;
     } else {
-      rows[ind + 1].style.display = "none";
+      return -1;
     }
-  });
-}
-
-function show_all(items) {
-  items.forEach(item => item.style.display = "");
+  }).filter(ind => ind >= 0);
 }
 
 function matches_all_searches(searchInputs, record) {
   return [...searchInputs].every(search => {
     return record[search.name].indexOf(search.value) >= 0;
   });
+}
+
+function render(pageSize, page = 0) {
+  const toShow = filter_table();
+  const pages = Math.ceil(toShow.length/pageSize);
+  showPagination(pages);
+  const thisPage = toShow.slice(pageSize*page, pageSize*(page + 1));
+  rows.forEach((row, ind) => {
+    if (thisPage.includes(ind)) {
+      row.style.display = "";
+    } else {
+      row.style.display = "none";
+    }
+  });
+}
+
+function showPagination(pages) {
+  const paginationItems = document.querySelectorAll(".page-item");
+  paginationItems.forEach((item, ind) => {
+    if (ind < pages) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
+function createPaginationLinks(data, pageSize) {
+  const pages = Math.ceil(data.length/pageSize);
+  for (let i=0; i<pages; i++) {
+    const item = document.createElement("li");
+    item.classList.add("page-item");
+    const link = document.createElement("a");
+    item.appendChild(link);
+    link.classList.add("page-link");
+    link.innerHTML = i + 1;
+    link.dataset.page = i;
+    link.addEventListener("click", () => render(pageSize, i));
+    paginationParent.appendChild(item);
+  }
 }
 
 function build_data_table(parent, data) {
@@ -76,6 +121,7 @@ function add_table(parent, columns) {
 
 function add_row(table, values) {
   const row = document.createElement("tr");
+  row.classList.add("data")
   let cell;
   values.forEach(value => {
     cell = document.createElement("td");
