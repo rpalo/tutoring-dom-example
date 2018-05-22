13d17ef11bc6ff16296b6ad93eaf02156ae9560f
Ryan Palo <ryan@thepalos.com>
2018-05-22 16:18:11 -0700

    Refactor to avoid global variables

--- index.js
+++ index.js
@@ -1,63 +1,51 @@
-const parent = document.querySelector(".table__parent");
 // fake_data.js provides data, an array of objects
 
-const PAGESIZE = 50;
+main(50, data);
 
-build_data_table(parent, data);
+function main(pageSize, dataset) {
+  const parent = document.querySelector(".table__parent");
+  buildDataTable(parent, dataset);
 
-
-// Get handles on inputs
-const dateSearch = document.querySelector('.search[name="timestamp"]');
-const commentSearch = document.querySelector('.search[name="comment"]');
-const selectSearches = document.querySelectorAll('select.search');
-const textSearches = document.querySelectorAll('input.search');
-const allSearches = document.querySelectorAll(".search");
-const clearButton = document.querySelector(".clear");
-const rows = document.querySelectorAll(".data");
-const paginationParent = document.querySelector(".pagination");
-
-createPaginationLinks(data, PAGESIZE);
-
-render(PAGESIZE);
-
-
-// Setup inputs and add event listeners
-textSearches.forEach(input => input.addEventListener("keyup", () => render(PAGESIZE)));
-
-selectSearches.forEach(select => {
-  const column = select.name;
-  const options = new Set(data.map(record => record[column]));
-  options.forEach(option => {
-    const optionElement = document.createElement("option")
-    optionElement.text = option;
-    select.options.add(optionElement);
+  // Get handles on inputs
+  const dateSearch = document.querySelector('.search[name="timestamp"]');
+  const commentSearch = document.querySelector('.search[name="comment"]');
+  const selectSearches = document.querySelectorAll('select.search');
+  const textSearches = document.querySelectorAll('input.search');
+  const allSearches = document.querySelectorAll(".search");
+  const clearButton = document.querySelector(".clear");
+  const rows = document.querySelectorAll(".data");
+  const paginationParent = document.querySelector(".pagination");
+  
+  // Generate pagination links and add functionality
+  const paginationLinks = createPaginationLinks(paginationParent, dataset, pageSize);
+  paginationLinks.forEach(link => {
+    link.addEventListener("click", () => render(rows, dataset, allSearches, pageSize, parseInt(link.dataset.page)));
   });
-  select.addEventListener("change", () => render(PAGESIZE));
-});
-
-clearButton.addEventListener("click", () => {
-  allSearches.forEach(search => search.value = "");
-  render(PAGESIZE);
-});
-
-function filter_table() {
-  return data.map((record, ind) => {
-    if (matches_all_searches(allSearches, record)) {
-      return ind;
-    } else {
-      return -1;
-    }
-  }).filter(ind => ind >= 0);
+  
+  // Setup inputs and add event listeners
+  textSearches.forEach(input => input.addEventListener("keyup", () => render(rows, dataset, allSearches, pageSize)));
+  
+  selectSearches.forEach(select => {
+    const column = select.name;
+    const options = new Set(data.map(record => record[column]));
+    options.forEach(option => {
+      const optionElement = document.createElement("option")
+      optionElement.text = option;
+      select.options.add(optionElement);
+    });
+    select.addEventListener("change", () => render(rows, dataset, allSearches, pageSize));
+  });
+  
+  clearButton.addEventListener("click", () => {
+    allSearches.forEach(search => search.value = "");
+    render(rows, dataset, allSearches, pageSize);
+  });
+  
+  render(rows, dataset, allSearches, pageSize);
 }
 
-function matches_all_searches(searchInputs, record) {
-  return [...searchInputs].every(search => {
-    return record[search.name].indexOf(search.value) >= 0;
-  });
-}
-
-function render(pageSize, page = 0) {
-  const toShow = filter_table();
+function render(rows, dataset, filters, pageSize, page = 0) {
+  const toShow = filterTable(filters, dataset);
   const pages = Math.ceil(toShow.length/pageSize);
   showPagination(pages);
   const thisPage = toShow.slice(pageSize*page, pageSize*(page + 1));
@@ -67,6 +55,22 @@ function render(pageSize, page = 0) {
     } else {
       row.style.display = "none";
     }
+  });
+}
+
+function filterTable(filterInputs, dataToMatch) {
+  return dataToMatch.map((record, ind) => {
+    if (matchesAllSearches(filterInputs, record)) {
+      return ind;
+    } else {
+      return -1;
+    }
+  }).filter(ind => ind >= 0);
+}
+
+function matchesAllSearches(filterInputs, record) {
+  return [...filterInputs].every(filter => {
+    return record[filter.name].indexOf(filter.value) >= 0;
   });
 }
 
@@ -81,8 +85,9 @@ function showPagination(pages) {
   });
 }
 
-function createPaginationLinks(data, pageSize) {
+function createPaginationLinks(parent, data, pageSize) {
   const pages = Math.ceil(data.length/pageSize);
+  const links = [];
   for (let i=0; i<pages; i++) {
     const item = document.createElement("li");
     item.classList.add("page-item");
@@ -91,26 +96,26 @@ function createPaginationLinks(data, pageSize) {
     link.classList.add("page-link");
     link.innerHTML = i + 1;
     link.dataset.page = i;
-    link.addEventListener("click", () => render(pageSize, i));
-    paginationParent.appendChild(item);
+    parent.appendChild(item);
+    links.push(link);
   }
+  return links;
 }
 
-function build_data_table(parent, data) {
+function buildDataTable(parent, data) {
   const titles = Object.keys(data[0]);
-  const table = add_table(parent, titles);
+  const table = addTable(parent, titles);
   data.forEach(row => {
-    add_row(table, Object.values(row));
+    addRow(table, Object.values(row));
   });
 }
 
-function add_table(parent, columns) {
+function addTable(parent, columns) {
   const table = document.createElement("table");
   table.classList.add("table", "table-striped", "sightings");
   const row = document.createElement("tr");
-  let cell = "";
   columns.forEach(title => {
-    cell = document.createElement("th");
+    const cell = document.createElement("th");
     cell.innerHTML = title;
     row.appendChild(cell);
   });
@@ -119,12 +124,11 @@ function add_table(parent, columns) {
   return table;
 }
 
-function add_row(table, values) {
+function addRow(table, values) {
   const row = document.createElement("tr");
   row.classList.add("data")
-  let cell;
   values.forEach(value => {
-    cell = document.createElement("td");
+    const cell = document.createElement("td");
     cell.innerHTML = value;
     row.appendChild(cell);
   });
